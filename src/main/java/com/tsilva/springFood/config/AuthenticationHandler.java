ade162fc0d7270119026b4ce9ef4782ae2f0bb0c
Telmo Silva <telmo.anastacio@gmail.com>
2020-06-18 13:49:30 +0200

    improved login mechanism

--- src/main/java/com/tsilva/springFood/config/AuthenticationHandler.java
+++ src/main/java/com/tsilva/springFood/config/AuthenticationHandler.java
@@ -2,11 +2,14 @@ package com.tsilva.springFood.config;
 
 import com.tsilva.springFood.entity.User;
 import com.tsilva.springFood.service.IUserService;
+import com.tsilva.springFood.utils.AuthorityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CsrfToken;
@@ -17,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Telmo Silva on 27.04.2020.
@@ -52,7 +56,27 @@ public class AuthenticationHandler implements AuthenticationSuccessHandler, Auth
 
 		CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
 		response.setHeader("_csrf", csrfToken.getToken());
-		response.setHeader("AUTHORITY", request.getAuthType());
+
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		List<String> authorityList = AuthorityUtils.getUserAuthorities(userDetails);
+		if(authorityList != null && !authorityList.isEmpty())
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append("");
+			for(int i = 0; i < authorityList.size(); i++)
+			{
+				if(i == authorityList.size() - 1)
+				{
+					sb.append(authorityList.get(i));
+				}
+				else
+				{
+					sb.append(authorityList.get(i));
+					sb.append(',');
+				}
+			}
+			response.setHeader("AUTHORITY", sb.toString());
+		}
 	}
 
 	@Override
